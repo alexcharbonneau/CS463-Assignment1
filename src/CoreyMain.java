a6c8f2c0c92df3d7e316cba2026bf0eb200ae22e
AcerPC <AcerPC@10.122.8.179>
2019-01-24 16:12:15 -0500

    Added 2D test arrays

--- src/CoreyMain.java
+++ src/CoreyMain.java
@@ -6,7 +6,47 @@ public class CoreyMain {
 	
 		
 		UserInterface coreyTest = new UserInterface();
+		
+		int[][] binaryImageDilation;
+		binaryImageDilation = new int[][]{   //Dilation test array
+			 {0,0,0,0},
+			 {0,1,1,0},
+			 {0,0,0,0}
+		};
+				
+		int[][] binaryImageErosion;
+		binaryImageErosion = new int [][] { //Erosion test array
+			{0,0,1,1,0},
+			{0,0,1,1,0},
+			{0,0,1,1,0},
+			{1,1,1,1,1}
+		};
+		
+		
+		
+		int[][] dilationStructure;
+		dilationStructure = new int [][] {
+			
+			{1},
+			{1},
+			{1,1}
+			
+		};
+		
+		int[][] erosionStructure;
+		erosionStructure = new int [][] {
+			{1},
+			{1},
+			{1}
+		};
+		
+		}
+		
+				
+	}		
+			
+			
+				
+	
 
-	}
 
-}
