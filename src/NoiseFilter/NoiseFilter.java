189bcc0596319065ce0196e085f0e4cd72bb7162
AcerPC <AcerPC@10.122.8.179>
2019-01-24 16:08:11 -0500

    Added 2D test arrays

--- src/NoiseFilter/NoiseFilter.java
+++ src/NoiseFilter/NoiseFilter.java
@@ -2,9 +2,43 @@ package NoiseFilter;
 
 public class NoiseFilter {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+	
+	/* 	dilate(B,S) takes binary image B, places the origin
+		of structuring element S over each 1-pixel, and ORs
+		the structuring element S into the output image at
+		the corresponding position.
+	
+		Structure:  1
+	               [1]1    [] = origin
+ 	---------------------------------------------------------------
+	
+	
+		erode(B,S) takes a binary image B, places the origin 
+		of structuring element S over every pixel position, and
+		ORs a binary 1 into that position of the output image 
+		only if every position of S (with a 1) covers a 1 in B.
+		
+		Structure: 1
+		          [1]  []= origin
+	               1
+	 */
+	
+	
+
+		
+		
+		public static void dilate(int[][] binaryImageDilation, int[][] dilationStructure){
+			
+			
+			
+			
+			
+			
+			
+			
+		}
 
 	}
 
-}
+
