9854f143e1325d12e502cf4bd0766a453232cdfe
alexcharbonneau <alexcharbonneau@users.noreply.github.com>
2019-01-23 22:58:35 -0500

    modifs

--- src/UserInterface/UserInterface.java
+++ src/UserInterface/UserInterface.java
@@ -1,5 +1,11 @@
+/** Author: Alexandre Charbonneau
+ ** Handles the initial file loading and processes it into a 2D array of int
+ **/
+
 package UserInterface;
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,8 +17,14 @@ public class UserInterface {
 	
 	private JFrame mainWindow;
 	private JPanel mainPanel;
+	private JButton submit;
+	private JLabel instructions;
+	private JTextField fileInputField;
 	
 	public UserInterface() {
+		fileInputField = new JTextField(35);
+		submit = new JButton("OK");
+		instructions = new JLabel("Please enter the image file path");
 		mainWindow = new JFrame();
 		mainWindow.setTitle("Object Detection Engine");
 		mainPanel = new JPanel();
@@ -20,9 +32,18 @@ public class UserInterface {
 		mainWindow.setVisible(true);
 		mainWindow.add(mainPanel);
 		mainPanel.setLayout(new BorderLayout());
-		mainPanel.add(new JLabel("Please enter the image file path"), BorderLayout.PAGE_START);
-		mainPanel.add(new JTextField(35), BorderLayout.WEST);
-		mainPanel.add(new JButton("OK"), BorderLayout.EAST);
+		mainPanel.add(instructions, BorderLayout.PAGE_START);
+		mainPanel.add(fileInputField, BorderLayout.WEST);
+		mainPanel.add(submit, BorderLayout.EAST);
 		mainWindow.pack();
-		}
+		submit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (!fileInputField.getText().equals(""))
+					instructions.setText("...loading image...");
+			}
+			
+		});
+	}
 }
