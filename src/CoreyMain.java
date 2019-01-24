8bcd9d38b0d9556b598581ca7f24086cb1a001eb
AcerPC <AcerPC@10.122.8.179>
2019-01-24 14:04:14 -0500

    Setting up my classes/main

---
+++ src/CoreyMain.java
@@ -0,0 +1,12 @@
+import UserInterface.UserInterface;
+
+public class CoreyMain {
+
+	public static void main(String[] args) {
+	
+		
+		UserInterface coreyTest = new UserInterface();
+
+	}
+
+}
---
+++ src/NoiseFilter/NoiseFilter.java
@@ -0,0 +1,10 @@
+package NoiseFilter;
+
+public class NoiseFilter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
