6c2d7d545753c4ff66efa5cfcca8153590ca7e6a
alexcharbonneau <alexcharbonneau@users.noreply.github.com>
2019-01-26 14:42:32 -0500

    more work on sig objects

--- src/AlexMain.java
+++ src/AlexMain.java
@@ -1,5 +1,7 @@
 import java.io.File;
 
+import NoiseFilter.NoiseFilter;
+import Threshold.Threshold;
 import UserInterface.UserInterface;
 
 public class AlexMain {
@@ -9,8 +11,22 @@ public class AlexMain {
 		UserInterface test = new UserInterface();
 		//int[][] A = {{0,1,1,0,1}, {0,1,1,0,1}, {0,1,1,0,1}, {0,1,1,0,1}, {0,1,1,0,1}, {0,1,1,0,1}, {0,1,1,0,1}, {0,1,1,0,1}, {0,1,1,0,1}};
 		//test.displayMatrix(A, 1);
-		String path = new String("src\\Resources\\Images\\image1.pgm");
+		String path = new String("src\\Resources\\Images\\image3.pgm");
 		int[][] converted = test.convertImage(new File(path));
+		
+		Threshold t = new Threshold();
+		test.displayMatrix(Threshold.PGMThreshold(converted, 127), 1);
+
+
+		UserInterface test3 = new UserInterface();
+		converted = NoiseFilter.dilate(converted);
+		test3.displayMatrix(converted, 1);
+
+		UserInterface test2 = new UserInterface();
+		converted = NoiseFilter.erode2(converted);
+		test2.displayMatrix(converted, 1);
+		
+		
 	}
 
 }
--- src/SignificantObjects/SignificantObjects.java
+++ src/SignificantObjects/SignificantObjects.java
@@ -1,5 +1,12 @@
 package SignificantObjects;
 
+import java.awt.Point;
+
+/* Author: Alexandre Charbonneau
+ * This module assumes that every object found in the matrix will have a different label
+ * labels must be an int
+ * */
+
 public class SignificantObjects {
 
 	public SignificantObjects() {
@@ -15,10 +22,16 @@ public class SignificantObjects {
 			}
 		}
 		
-		for (int i = 0; i < count.length; i++) {
-			int y, x;
-			if (count[i] >= 20) {
+		for (int i = 0; i < count.length; i++)	//finding how many objects are significant
+			if (count[i] >= 20)
 				objectCount++;
+		
+		ObjectDetails[] resultList = new ObjectDetails[objectCount];	//building the array which contains the objectdetails
+		
+		for (int i = 0; i < count.length; i++) {
+			int y = 0;
+			int x = 0;
+			if (count[i] >= 20) {
 				boolean found = false;
 				int row = 0;
 				int col = 0;
@@ -35,17 +48,77 @@ public class SignificantObjects {
 						}
 					}
 				}
-				y = row;
+				y = col;
 				found = false;
 				row = 0;
 				col = 0;
 				while (!found) {
-					
+					if (A[row][col] == count[i]) {
+						found = true;
+					}
+					else {
+						row++;
+						if (row == A.length) {
+							row = 0;
+							col++;
+						}
+					}
 				}
+				x = row;
+				found = false;
+			
+				//starting from bottom right to find the other extremity
+				
+				int bottomx = 0;
+				int bottomy = 0;
+				
+				row = A.length - 1;	//reseting those variables
+				col = A[0].length - 1;
+				
+				while (!found) {
+					if (A[row][col] == count[i]) {
+						found = true;
+					}
+					else {
+						col--;
+						if (col == -1) {
+							col = A[0].length - 1;
+							row--;
+						}
+					}
+				}
+				
+				bottomy = row;
+				found = false;
+				
+				row = A.length - 1;	//reseting those variables
+				col = A[0].length - 1;
+				
+				while (!found) {
+					if (A[row][col] == count[i]) {
+						found = true;
+					}
+					else {
+						row--;
+						if (row == -1) {
+							row = A[0].length - 1;
+							col--;
+						}
+					}
+				}
+				
+				bottomx = col;
+				
+				int[][] pixels = new int[bottomx - x + 1][ bottomy - y + 1];
+				resultList[i] = new ObjectDetails(new Point(x, y), pixels);
+			
 			}
+
+			
 		}
 		
-		return null;
+		
+		return resultList;
 		
 	}
 }
