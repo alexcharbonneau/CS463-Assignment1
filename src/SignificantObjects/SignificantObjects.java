efbf744fb1e2cad9b374a31f1e66ac035c5d3310
alexcharbonneau <alexcharbonneau@users.noreply.github.com>
2019-01-25 22:10:08 -0500

    sig object

--- src/SignificantObjects/SignificantObjects.java
+++ src/SignificantObjects/SignificantObjects.java
@@ -8,8 +8,23 @@ public class SignificantObjects {
 	
 	public ObjectDetails[] getObjects(int[][]A) {
 		
+		int[] count = new int[A.length * A[0].length];	//making an array to count label size
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A[0].length; j++) {
+				count[A[i][j]]++;
+			}
+		}
 		
-		
+		for (int i = 0; i < count.length; i++) {
+			int y, x;
+			if (count[i] >= 20) {
+				for (int j = 0; j < A.length; j++) {
+					for (int k = 0; k < A[0].length; k++) {
+						
+					}
+				}
+			}
+		}
 		
 		return null;
 		
