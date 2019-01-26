27f6ed506938be52ffd246c7dea9697f8e315e41
lee <lee@macbook-air.local>
2019-01-26 12:43:51 -0500

    added module threshold

---
+++ src/LeeMain.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+import UserInterface.UserInterface;
+import Threshold.Threshold;
+
+
+public class LeeMain {
+	
+	//testing purpose only (hardcoded)
+	public static void main(String[] args) {
+		UserInterface test = new UserInterface();
+		int[][] convertedImage = test.convertImage(new File("src/resources/images/image5.pgm")).clone();
+		Threshold threshold = new Threshold();
+		int[][] binaryImage = threshold.PGMThreshold(convertedImage, 127).clone();
+		test.displayMatrix(binaryImage, 1);
+		
+	}
+}
