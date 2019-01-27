11abf4870331c5a8ea32891da30d931855cd2e23
Tyler <Tyler@10.122.19.33>
2019-01-27 16:28:15 -0500

    program is able to count the different components by using internal/external corners. It also labels the different components with labels from 1 to n

--- src/ObjectLabelling/ObjectLabelling.java
+++ src/ObjectLabelling/ObjectLabelling.java
@@ -1,5 +1,131 @@
 package ObjectLabelling;
 
+import java.util.Arrays;
+
 public class ObjectLabelling {
 
+	public static int[][] countGroups(int[][] m) {
+
+	    
+	    int row = m.length;
+	    int col = m[0].length;
+	    int eCorners = 0;
+	    int iCorners = 0;
+	    int numObjects = 0;
+	    int labelCount = 1;
+	    
+	    for (int i=0; i<m.length-1; i++) {
+	    	for (int j=0; j<m[0].length-1; j++) {
+	    		//for counting external corners
+	    		if (m[i][j]==1 && m[i][j+1]==1 && m[i+1][j]==1 && m[i+1][j+1]==0) {
+	    			eCorners++;
+	    		}
+	    		if (m[i][j]==1 && m[i][j+1]==1 && m[i+1][j]==0 && m[i+1][j+1]==1) {
+	    			eCorners++;
+	    		}
+	    		if (m[i][j]==1 && m[i][j+1]==0 && m[i+1][j]==1 && m[i+1][j+1]==1) {
+	    			eCorners++;
+	    		}
+	    		if (m[i][j]==0 && m[i][j+1]==1 && m[i+1][j]==1 && m[i+1][j+1]==1) {
+	    			eCorners++;
+	    		}
+	    		//for counting internal corners
+	    		if (m[i][j]==0 && m[i][j+1]==0 && m[i+1][j]==0 && m[i+1][j+1]==1) {
+	    			iCorners++;
+	    		}
+	    		if (m[i][j]==0 && m[i][j+1]==0 && m[i+1][j]==1 && m[i+1][j+1]==0) {
+	    			iCorners++;
+	    		}
+	    		if (m[i][j]==0 && m[i][j+1]==1 && m[i+1][j]==0 && m[i+1][j+1]==0) {
+	    			iCorners++;
+	    		}
+	    		if (m[i][j]==1 && m[i][j+1]==0 && m[i+1][j]==0 && m[i+1][j+1]==0) {
+	    			iCorners++;
+	    		}
+	    		
+	    	}
+	    }
+	    numObjects = (iCorners-eCorners)/4;
+	    
+//	    System.out.println(eCorners);
+//	    System.out.println(iCorners);
+//	    System.out.print(numObjects);
+
+	    //turning 1's in the array into -1's
+	    for(int i=0; i<m.length; i++) {
+	    	for(int j=0; j<m[0].length; j++) {
+	    		if(m[i][j]==1) {
+	    			m[i][j]=-1;
+	    		}
+	    	}
+	    }
+
+	    FindComponents(m, labelCount);
+	    
+    
+	    return m;
+	    
+	}
+	
+    public static void FindComponents(int[][]x, int y) {
+    	for(int i=0; i<x.length; i++) {
+    		for(int j=0; j<x[0].length; j++) {
+    			if(x[i][j]==-1) {
+    				x[i][j]=y;
+    				ConnectedComponents(x, i, j);
+    				y++;
+    			}
+    		
+    		}
+    	}
+    	PrintArray(x);
+    }
+    
+    public static void ConnectedComponents(int[][]x, int i, int j) {
+    	if(i>0) {
+    		if(x[i-1][j]!=0) {
+    			x[i-1][j]=x[i][j];
+    		}
+    	}
+    	
+       	if(j>0) {
+    		if(x[i][j-1]!=0) {
+    			x[i][j-1]=x[i][j];
+    		}
+    	}
+       	
+    	if(i<x.length-1) {
+    		if(x[i+1][j]<0) {
+    			x[i+1][j]=x[i][j];
+    		}
+    	}
+    	
+    	
+    	if(j<x[0].length-1) {
+    		if(x[i][j+1]<0) {
+    			x[i][j+1]=x[i][j];
+    		}
+    	}
+    	
+    	if(i<x.length-1 && x[i+1][j]>0) {
+    		ConnectedComponents(x, i+1, j);
+    	}
+    	
+    	if(j<x[0].length-1 && x[i][j+1]>0) {
+    		ConnectedComponents(x, i, j+1);
+    	}
+    	
+
+    }
+    public static void PrintArray(int[][]m) {
+    	for(int i=0; i<m.length; i++) {
+	    	for(int j=0; j<m[0].length; j++) {
+	    		System.out.print(m[i][j] + " ");
+	    	}
+	    	System.out.println();
+	    }
+	
+    }
+    
+    
 }
--- src/TylerMain.java
+++ src/TylerMain.java
@@ -1,9 +1,25 @@
+import ObjectLabelling.ObjectLabelling;
+import UserInterface.UserInterface;
 
 public class TylerMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[][]testArray;     // 2nd Dilation test array
+		testArray = new int [][] { 
+			{1,1,1,1,1,1,1,1,1},
+			{0,1,1,0,1,1,1,0,1},
+			{0,0,0,0,1,1,1,0,1},
+			{0,0,0,0,0,0,0,0,1},
+			{0,0,1,1,1,1,1,0,1},
+			{0,0,0,1,1,1,1,0,1},
+			{0,1,1,1,0,0,0,0,1},
+			{0,0,0,0,0,0,0,0,1},
+		};
+		
+		ObjectLabelling tester = new ObjectLabelling();
+		tester.countGroups(testArray);
+		
 	}
 
 }
